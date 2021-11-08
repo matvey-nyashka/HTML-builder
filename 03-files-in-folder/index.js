54b1014d6f5bfc2e13c0de101bf714049d56a8b4
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-11-09 01:15:55 +0300

    feat: added third task solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+
+let writePath = path.join(__dirname, 'secret-folder');
+fs.readdir(writePath, (err, files) => {
+    if (err) throw err;
+    for (let i=0; i<files.length; i++){
+      fs.stat(writePath + "/" + files[i], (err, stats) => {
+        if (err) throw err;
+        if (stats.isFile()) {
+          console.log(files[i].replace(".", " - ") + " - " + (stats.size / 1024) + " kb")
+        }
+      });
+   }
+  }); 
