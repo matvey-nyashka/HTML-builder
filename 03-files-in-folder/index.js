6a697d7bf64a87a42e09a9ce1050f706bd0e8680
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-11-09 15:12:15 +0300

    feat: added four task solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,7 +5,7 @@ let writePath = path.join(__dirname, 'secret-folder');
 fs.readdir(writePath, (err, files) => {
     if (err) throw err;
     for (let i=0; i<files.length; i++){
-      fs.stat(writePath + "/" + files[i], (err, stats) => {
+      fs.stat(path.join(writePath, files[i]), (err, stats) => {
         if (err) throw err;
         if (stats.isFile()) {
           console.log(files[i].replace(".", " - ") + " - " + (stats.size / 1024) + " kb")
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+let writePath= path.join(__dirname, 'files');
+let copyPath= path.join(__dirname, 'files-copy');
 
+fs.mkdir(copyPath, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+fs.readdir(copyPath, (err, files) => {
+    if (err) throw err;
+    for (let i=0; i<files.length; i++){
+      fs.unlink(path.join(copyPath, files[i]), (err) => {
+        if (err) throw err;
+      });
+    };
+  });  
+
+fs.readdir(writePath, (err, files) => {
+    if (err) throw err;
+    for (let i=0; i<files.length; i++){
+      fs.copyFile(path.join(writePath,files[i]), path.join(copyPath, files[i]), (err) => {
+        if (err) throw err;
+      });
+    };
+  });
